4d2a351750770fbf9525eb578804643e7199ef67
Avi Avinav <aviavinav2004@gmail.com>
2020-11-01 05:10:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''Eel is basically a module to create gui interfaces for python using HTML, CSS and javascript.
+ It is used to give a website lke interface to a desktop app'''
+
+import eel
+eel.init('web')  #Initializing eel
+eel.start('index.html')  #giving the html file
+
+
+'''Leave the HTML side to me.'''
